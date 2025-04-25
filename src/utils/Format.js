ef33c961f1360b8d1b0adc7410cb36b10ae12b96
Lucas-GES <lucas.espiritosanto@edu.unifil.br>
2025-04-24 21:58:25 -0300

    Importando e Exportando módulos com Webpack

--- src/utils/Format.js
+++ src/utils/Format.js
@@ -1,4 +1,4 @@
-class Format {
+export class Format {
     // Método recebe os ids existentes no html cria um elemento <div>
     // Após criar o elemento com um dataset recebendo os ids, é retornado a div com seu primeiro nó
     // com todas as chaves utilizando o Object.keys para pegalas do elemento;
